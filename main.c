92539ffd2583655b5b52e81ec29dfcafddef239c
Mihir <mihir2000puranik@gmail.com>
2021-01-30 14:29:02 +0800

    Updated Structure Comments

--- main.c
+++ main.c
@@ -7,40 +7,40 @@ int main (void){
 
     ///////// STRUCTURE  //////////////
 
+    // HOME SCREEN
+    // press 1: add question
+    // press 2: delete question
+    // press 3: take quiz
 
+    // press 1: add question
     // Read text from user for question(s)
+    // Read in 4 possible answers; 1 is the correct answer;
+    // Ask user for option A, option B etc.
+    // Ask user for correct option
+    // either continue adding or stop adding questions
 
-    // Read in 4 possible answers; 1 is the correct answer
 
-    // EOF to end adding questions
-
+    // press 3: take quiz
     // Starts quiz
-        // A, B, C, D to select answer.
-        // Right answer: correct
+        // A, B, C, D to select answer; scanf users option;
+        // Right answer: correct; +1 to score counter
         // Wrong answer: incorrect, show answer, next question, no second chance
-        // try to randomise the answers
 
-    //
+        // TRY to randomise the answers
 
+    // Show score
+        // number of questions = total score
+        // Calculate Score;
+        // track right answers;
+        // after last question of quiz, show score
+        // back to home screen; resets score
 
-
-}
-
-
-int function_1(void){
-
-    //Mihir's function
 
 }
 
-int function {
+
+int  {
+
     ???
     return 0;
-}
-
-
-add(){
-
-
-
 }
